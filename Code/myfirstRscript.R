6b064978694fa70cc018bf9a81d2f99bac36889e
Susanna Casarini <104819186+SusannaCasarini@users.noreply.github.com>
2024-10-09 15:47:24 +0200

    Update myfirstRscript.R

--- Code/myfirstRscript.R
+++ Code/myfirstRscript.R
@@ -5,5 +5,7 @@ can <- c(10,20,30,50,70) # cananbis sativa
 dol <- c(1,3,5,7,9) # dolphins
 
 # Is there a correlation between cannabis and dolphins?
+plot(can,dol,pch=19,col="red")
 cor(can,dol)
+
 
