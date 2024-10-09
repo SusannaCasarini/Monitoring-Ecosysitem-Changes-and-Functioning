15b1877a288492281256c588316375651eb56826
Susanna Casarini <104819186+SusannaCasarini@users.noreply.github.com>
2024-10-09 15:39:44 +0200

    Create myfirstRscript.R

---
+++ Code/myfirstRscript.R
@@ -0,0 +1,9 @@
+# Array
+# a series of index, a sets of index
+
+can <- c(10,20,30,50,70) # cananbis sativa
+dol <- c(1,3,5,7,9) # dolphins
+
+# Is there a correlation between cannabis and dolphins?
+cor(can,dol)
+
